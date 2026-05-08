70dae19bcb2f91bd6e2441921213bbc85611a668
YoungSeo1104 <dudtj3116@naver.com>
2026-05-08 14:32:24 +0900

    init: turborepo 및 nest, next 초기 세팅

---
+++ apps/api/src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module, Global } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "../../drizzle/schema";
+
+export const DRIZZLE = Symbol("DRIZZLE");
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const url = configService.getOrThrow<string>("DATABASE_URL");
+        const client = postgres(url);
+        return drizzle(client, { schema });
+      },
+    },
+  ],
+  exports: [DRIZZLE],
+})
+export class DatabaseModule {}
---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,12 @@
+import baseConfig from "./index.js";
+
+export default [
+  ...baseConfig,
+  {
+    rules: {
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+  },
+];
