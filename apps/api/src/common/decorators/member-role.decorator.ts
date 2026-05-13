7b580918dddf61628475ab8cff222c10cade8175
Esther <33364524+lareina7486@users.noreply.github.com>
2026-05-13 13:39:03 +0900

    Revert "feat: auth Guard 6종 + common/ 인프라 정합"

--- apps/api/src/common/decorators/member-role.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { MemberRole } from '../enums/member-role.enum';
-
-export const MEMBER_ROLE_KEY = 'memberRoles';
-
-export const RequireMemberRole = (...roles: MemberRole[]) =>
-  SetMetadata(MEMBER_ROLE_KEY, roles);
