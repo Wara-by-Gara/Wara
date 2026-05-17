15853be61b1708e459288e90e5eafd05270a018b
ZoeYoon <bus1990@naver.com>
2026-05-17 16:33:12 +0900

    chore: 로컬 테스트 및 환경 설정

--- apps/api/src/photos/photos.module.ts
+++ apps/api/src/photos/photos.module.ts
@@ -3,9 +3,10 @@ import { PhotosController } from './photos.controller';
 import { PhotosService } from './photos.service';
 import { PhotosRepository } from './photos.repository';
 import { S3Module } from '../s3/s3.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [S3Module],
+  imports: [S3Module,AuthModule],
   controllers: [PhotosController],
   providers: [PhotosService, PhotosRepository],
 })
