2a63c5991c68b3f02579e63fc9fdfd8aae6764ca
Minsung Kim <alstjddl0513@gmail.com>
2026-06-11 13:12:00 +0900

    feat(web): 사진 지도 명칭을 "Place log"로 통일
    PR #226의 Storybook 섹션명과 일치시킴.

--- apps/web/src/screens/MyPage/MyPage.tsx
+++ apps/web/src/screens/MyPage/MyPage.tsx
@@ -186,7 +186,7 @@ export const MyPage = ({
               onClick={onPhotoMap}
               rightSlot={<Icon name="chevron-right" size="sm" color="inactive" decorative />}
             >
-              내 추억 지도
+              Place log
             </MenuItem>
             <MenuItem
               leftIcon="user-x"
